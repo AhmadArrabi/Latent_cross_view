bf996f4c0a48f05721124560fae5e397ea20c151
Xiaohan Zhang <xiaohan.zhang@uvm.edu>
2023-04-19 22:59:41 -0400

    fix relative path and finish dataloader

--- Create_JSON.py
+++ Create_JSON.py
@@ -1,21 +1,22 @@
 import json
 import os
 
-mode = 'train'
+mode = 'val'
+
 
 CSV_splits = f"/gpfs2/scratch/xzhang31/CVUSA/dataset/splits/{mode}-19zl.csv"
-SOURCE_DIR = "/gpfs2/scratch/xzhang31/CVUSA/dataset/"
-TARGET_DIR = "/gpfs2/scratch/xzhang31/CVUSA/dataset/"
+SOURCE_DIR = "CVUSA/dataset/"
+TARGET_DIR = "CVUSA/dataset/"
 PROMPT = "Photo-realistic aerial-view image with high quality details."
 JSON_DIR = "./Data/"
 
-with open(f'{JSON_DIR}{mode}_prompt.txt', 'a') as f:
+with open(f'{JSON_DIR}{mode}_prompt.txt', 'w') as f:
     with open(CSV_splits, 'r') as p:
         for i in p.readlines():
             temp_dict = {}
             i = i.strip().split(',')
             temp_dict['source'] = os.path.join(SOURCE_DIR, i[1])
-            temp_dict['target'] = os.path.join(TARGET_DIR, i[1])
+            temp_dict['target'] = os.path.join(TARGET_DIR, i[0])
             temp_dict['prompt'] = PROMPT
             f.write(json.dumps(temp_dict))
             f.write('\n')
